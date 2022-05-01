6859574a1af39c9e20bf3143f3005563033ea32b
diplg <dimas4354@icloud.com>
2022-05-01 03:05:22 +0300

    задание 2

---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+Console.Clear();
+
+void Method1(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] a1 = new int [8];
+
+Console.WriteLine();
+
+for (int i = 0; i < a1.Length; i++)
+{
+    a1[i] = new Random().Next(-9, 9);
+}
+Method1(a1);
+for (int i = 0; i < a1.Length; i++)
+{
+    a1[i] = -a1[i];
+}
+Method1(a1);
