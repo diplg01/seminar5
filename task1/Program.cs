00e338ebdfc21e217b98b996e15dfd05df72a699
diplg <dimas4354@icloud.com>
2022-05-01 03:04:55 +0300

    задание 1

---
+++ task1/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Print(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+int [] mass = new int [12];
+
+Print(mass);
+
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(-9, 10);
+}
+
+Print(mass);
+
+int summPos = 0;
+int summNeg = 0;
+
+for (int i = 0; i < mass.Length; i++)
+{
+    if(mass[i] > 0)
+    {
+        summPos += mass[i];
+    }
+    else                                          //(mass[i] < 0)
+     {
+        summNeg += mass[i];
+     }
+}
+Console.WriteLine($"Сумма положительнфх чисел {summPos} и трицательных чисел {summNeg}");
